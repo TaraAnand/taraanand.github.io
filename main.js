9577b61e4ca347e929cb3f90e378778ee4d29f10
TaraAnand <ta2432@barnard.edu>
2018-08-20 21:30:45 -0400

    javascript file

--- main.js
+++ main.js
@@ -1,3 +1,57 @@
+/*var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
+*/
+
+var slideIndex = [1,1];
+var slideId = ["slide", "slide2"]
+showDivs(1, 0);
+showDivs(1, 1);
+
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function slider() {
     var slides = $('.main-slide');
     var i = 0;
