0cb1bfb5c2352e2fad723b1583981a3bf9218fb5
TaraAnand <ta2432@barnard.edu>
2018-12-28 15:41:24 -0500

    Dynamic nav buttons

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ function showDivs(n, no) {
   if(slideIndex[no]-1 == 0){
 
   }
-
 }
 
 function slider() {
@@ -100,38 +99,41 @@ function slider() {
     });
 }
 
-function modal() {
-    $('a[href*=#]:not([href^=#open], [href^=#close])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 500);
-                return false;
-            }
-        }
-    });
-}
 
-function alternate() {
-    $('.alternate img:gt(0)').hide();
-    setInterval(function () {
-        $('.alternate :first-child').fadeOut().next('img').fadeIn().end().appendTo('.alternate');
-    }, 1000);
-}
-
-function gifSwitch() {
-    $('.switch img:gt(0)').hide();
-    setInterval(function () {
-        $('.switch :first-child').hide().next('img').show().end().appendTo('.switch');
-    }, 2000);
-}
+function scroll(){
+  // $sections incleudes all of the container divs that relate to menu items.
+  var $sections = $('.sectionHeader');
+  
+  // The user scrolls
+  $(window).scroll(function(){
+    
+    // currentScroll is the number of pixels the window has been scrolled
+    var currentScroll = $(this).scrollTop();
+    
+    // $currentSection is somewhere to place the section we must be looking at
+    var $currentSection
+    
+    // We check the position of each of the divs compared to the windows scroll positon
+    $sections.each(function(){
+      // divPosition is the position down the page in px of the current section we are testing      
+      var divPosition = $(this).offset().top;
+      
+      // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
+      // the -1 is so that it includes the div 1px before the div leave the top of the window.
+      if( divPosition - 1 < currentScroll ){
+        // We have either read the section or are currently reading the section so we'll call it our current section
+        $currentSection = $(this);
+        // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
+      }
+      // This is the bit of code that uses the currentSection as its source of ID
+      var id = $currentSection.attr('id');
+     $('div').removeClass('sectionHeaderActive');
+     $("[href=#"+id+"]").addClass('sectionHeaderActive');
+    })
+  });
+};
 
 $(document).ready(function () {
     slider();
-    modal();
-    alternate();
-    gifSwitch();
+    scroll();
 });
